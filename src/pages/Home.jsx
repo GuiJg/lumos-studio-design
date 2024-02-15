e3fbf3d534c44db6ab191685a8b10b0b47ed8b6d
GuiJg <j.gu1lh3rm325@gmail.com>
2024-02-14 22:09:57 -0300

    test: testing build error

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,20 @@
 // import BannerSection from '../sections/BannerSection.js';
-import AboutSection from '../sections/AboutSection.js';
-import ServicesSection from '../sections/ServicesSection.js';
-import ProjectSection from '../sections/ProjectSection.js';
-import ContactSection from '../sections/ContactSection.js';
-import Footer from '../sections/footer.js';
+// import AboutSection from '../sections/AboutSection.js';
+// import ServicesSection from '../sections/ServicesSection.js';
+// import ProjectSection from '../sections/ProjectSection.js';
+// import ContactSection from '../sections/ContactSection.js';
+// import Footer from '../sections/footer.js';
 import '../style.css';
 
 function Home() {
     return (
         <div className="App">
             {/* <BannerSection/> */}
-            <AboutSection/>
+            {/* <AboutSection/>
             <ServicesSection/>
             <ProjectSection/> 
             <ContactSection/>
-            <Footer/> 
+            <Footer/>  */}
         </div>
     );
 }
