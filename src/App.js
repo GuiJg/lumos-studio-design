62a189a02a4d6e4e4a92e33c5a7a9bf01741bbe1
GuiJg <j.gu1lh3rm325@gmail.com>
2024-02-16 15:12:24 -0300

    fix: update nodejs to resolve error

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import { Outlet } from 'react-router-dom' 
-import NavBar from './pages/navbar'
-import Header from './pages/header'
+import NavBar from './pages/Navbar'
+import Header from './pages/Header'
 
 import './style.css'
-import Footer from './sections/footer'
+import Footer from './sections/Footer'
 
 function App(){
 
